6fa2f21501af1553ae77d8e96d5576ffbcda186b
49985 <amoiz222862@gmail.com>
2021-04-18 16:07:06 +0500

    my first java attempt

---
+++ app.js
@@ -0,0 +1,213 @@
+
+
+
+// *********************************CHAPTER 4*******************************
+
+
+
+// *************legal variables******************
+
+// var name = "Abdul Moiz"
+// var fatherName = "Zahid Hussain"
+// var roll_no = 21168
+// var occupation = "student"
+// var University_name = "IQRA University"
+
+
+
+
+// **************illegal variables*************
+
+// var full.name = "abdul moiz zahid"
+// var father name = "zahid hussain"
+// var roll-number = 21168
+// var phone/other = "IU"
+// var  section Name = "B"
+
+
+
+
+
+
+
+// ********************************CHAPTER 5******************************
+
+
+// ________________________task 1__________________
+// var a = 3;
+// var b = 5;
+// document.write(a+b);
+
+
+// _________________________task 2___________________
+// var a = 5;
+// var b = 3;
+// document.write(a-b);
+
+// var a = 5;
+// var b = 3;
+// document.write(a*b);
+
+// var a = 5;
+// var b = 3;
+// document.write(a/b);
+
+
+// _________________________task 3________________________
+
+// var math;
+// math = 12;
+// document.write("value after decleration is : " + " " + math +"<br>");
+// document.write("initial value is :" +" " + math +"<br>")
+// ++math;
+// document.write("value after increment is:" + " " + math +"<br>");
+// math = math + 7;
+// document.write("after addition the 7 the value is:" + " " + math + "<br>");
+// --math;
+// document.write("value after 1 decrement is : " + " " + math + "<br>");
+// math = math % 13;
+// document.write("value of reminder  after divition by 13 is:" + " " + math + "<br>");
+// --math;
+// document.write("value after 1 decreament is:" + " " + math + "<br>");
+// --math;
+// document.write("value after 1 decreament is:" + " " + math + "<br>");
+// math = math % 4;
+// document.write("teminder after division by 4 is:" + " " + math);
+
+
+
+
+// ________________________task 4_________________________
+
+
+// var ticket = 600;
+// var howTicket = +prompt("how much you wnats tickets");
+// document.write("<h1> the cost of " + " " + howTicket + " " + "tickets is:");
+// var total = ticket * howTicket;
+// document.write(total + "<\h1>");
+
+
+
+
+// _________________________task 5_______________________________
+
+// document.write("<h1>Table of 4</h1>")
+
+// var table = 4
+
+// document.write("<h2>" + table + "x" + 1 + "=" + table*1 + "</h2>")
+// document.write("<h2>" + table + "x" + 2 + "=" + table*2 + "</h2>")
+// document.write("<h2>" + table + "x" + 3 + "=" + table*3 + "</h2>")
+// document.write("<h2>" + table + "x" + 4 + "=" + table*4 + "</h2>")
+// document.write("<h2>" + table + "x" + 5 + "=" + table*5 + "</h2>")
+// document.write("<h2>" + table + "x" + 6 + "=" + table*6 + "</h2>")
+// document.write("<h2>" + table + "x" + 7 + "=" + table*7 + "</h2>")
+// document.write("<h2>" + table + "x" + 8 + "=" + table*8 + "</h2>")
+// document.write("<h2>" + table + "x" + 9 + "=" + table*9 + "</h2>")
+// document.write("<h2>" + table + "x" + 10 + "=" + table*10 + "</h2>")
+
+// table of 4 end
+
+
+
+
+
+
+// ______________________________task 6__________________________
+
+// var farenhight= +prompt("Enter farehight value to convert in celcius");
+// celcius = (farenhight-32)*5/9;
+// document.write(" <h1> THE" +" " + farenhight + " " +"farenhight is in celcius is:" + " " + celcius +"<br> </h1>");
+// var cel= +prompt("Enter celcius value to convert in farenhight");
+// far = (cel* 9 / 5) +32;
+// document.write(" <h1> THE" +" " + cel + " " +"celcius is in faranhight is:" + " " + far +"<br> </h1>");
+
+
+
+// _________________________________task 9__________________________________
+
+// var usDollar = +prompt("HOW MANY YOU HAVE US DOLLOARS?");
+// var saudiRIYAL = +prompt("how many you have saudi riyals");
+// usTOpaki= usDollar*104.8;
+// saudiTOpaki = saudiRIYAL * 28;
+// document.write("The " + usDollar + " " + "dollars in pakistani rupees:" + " " + usTOpaki + "<br>");
+// document.write("The " + saudiRIYAL + " " + "saudiriyals in pakistani rupees:" + " " + saudiTOpaki + "<br>");
+
+
+// _____________________________task 12______________________________________
+
+// document.write("<h1>The Geometrizer</h1>")
+// var radius= +prompt("enter radius of circule");
+// var pi = 3.142;
+// var circumstance;
+// var area;
+// circumstance = 2 * pi * radius;
+// document.write("The circumstance of a circle is :" + " " + circumstance +"<br>");
+// area = pi * radius*radius;
+// document.write("The area of circle =" + " " + area );
+
+
+// _____________________________________task 13____________________________
+
+// var favSNAKE = prompt("enter your favorite snake");
+// var currentAGE = +prompt("ENTER YOUR CURRENT AGE");
+// var maxAGE = +prompt("enter your max age");
+// var amountPERday = +prompt("enter amount per day:");
+// var totalEXPENCE = (currentAGE+maxAGE)*amountPERday;
+// document.write("<h1> The life time supply calculator </h1> <br>");
+// document.write("your favorite snake is:" + " " + favSNAKE + "<br>");
+// document.write("YOUR CURRENT AGE is : " + " " + currentAGE + "<br>");
+// document.write("Amount per day is : " + " " + amountPERday + "<br>");
+// document.write("you will need to " + " " + totalEXPENCE + " " + favSNAKE + " " + "to last you until the reach old age of " + " " + maxAGE);
+
+
+
+
+
+
+
+
+
+
+
+// ________________________________Chapter 6_____________________________
+
+// __________________________task 1__________________________
+
+// var a = 10;
+// var result= ++a;
+// document.write("<h1>" + "Result:" + "</h2>")
+// document.write( "<h2>" + "if we are  doing ++a (pre increment)"+ " " + "then" + "  " + "the value of a is" + " " + result  )
+
+// var a = 10;
+// var result= --a;
+// document.write( "<h2>" + "if we are  doing --a (pre decrement)"+ " " + "then" + "  " + "the value of a is" + " " + result  )
+
+
+// var a = 10;
+// var result= a--;
+// document.write( "<h2>" + "if we are  doing a-- (post decrement)"+ " " + "then" + "  " + "the value of a is" + " " + result  )
+
+
+// var a = 10;
+// var result= a++;
+// document.write( "<h2>" + "if we are  doing a++ (post decrement)"+ " " + "then" + "  " + "the value of a is" + " " + result  )
+
+
+
+
+// _________________________________task 2_______________________________
+
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// document.write(a +"\t" + b  + "\t" + result );
+
+
+// _____________________________task 3____________________________
+
+
+// var name = prompt("Enter your name");
+// document.write (naam + " " + "ASSALAM U ALIKUM WARAHMATULLAH HI WABARAKHTHU:");
+
+
+
